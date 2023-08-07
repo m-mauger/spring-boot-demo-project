4ef4ac3fc5ece47c2aa2d756de3274eaf3367750
m-mauger <mm98business@gmail.com>
2023-08-06 19:17:14 -0500

    added enhancements and modifcations for prodcution on railway

--- src/main/java/dev/matthew/contentcalendar/model/Content.java
+++ src/main/java/dev/matthew/contentcalendar/model/Content.java
@@ -2,6 +2,7 @@ package dev.matthew.contentcalendar.model;
 
 import jakarta.validation.constraints.NotBlank;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Table;
 
 import java.time.LocalDateTime;
@@ -11,6 +12,7 @@ public record Content(
         Integer id,
         @NotBlank
         String title,
+        @Column(value = "description")
         String desc,
         Status status,
         Type contentType,
